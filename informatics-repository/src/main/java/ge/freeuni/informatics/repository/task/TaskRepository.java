193eb052fb81b1ee5c94e90acca8e783172027d5
Vano Gamezardashvili <vgame18@freeuni.edu.ge>
2022-08-27 01:01:04 +0400

    bugfix saving task

--- informatics-repository/src/main/java/ge/freeuni/informatics/repository/task/TaskRepository.java
+++ informatics-repository/src/main/java/ge/freeuni/informatics/repository/task/TaskRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
-import java.util.List;
 
 @Repository
 @Transactional

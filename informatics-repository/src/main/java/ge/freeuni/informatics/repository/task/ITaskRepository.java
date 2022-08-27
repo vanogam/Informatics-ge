965e9f726d7689237e4ea3fd1452608fe3e1314d
Vano Gamezardashvili <vgame18@freeuni.edu.ge>
2022-08-27 10:28:24 +0400

    bugfix adding new task

--- informatics-repository/src/main/java/ge/freeuni/informatics/repository/task/ITaskRepository.java
+++ informatics-repository/src/main/java/ge/freeuni/informatics/repository/task/ITaskRepository.java
@@ -3,11 +3,9 @@ package ge.freeuni.informatics.repository.task;
 import ge.freeuni.informatics.common.model.task.Task;
 import ge.freeuni.informatics.common.model.task.TestCase;
 
-import java.util.List;
-
 public interface ITaskRepository {
 
-    void addTask(Task task);
+    Task addTask(Task task);
 
     void saveTestcase(TestCase testCase);
 

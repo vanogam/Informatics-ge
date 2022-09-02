af4c5bf20f8a55741c3a7e68bb0ff8468076988c
Vano Gamezardashvili <vgame18@freeuni.edu.ge>
2022-09-02 22:23:02 +0400

    bugfix submission fields

---
+++ informatics-system/src/main/java/ge/freeuni/informatics/system/configuration/ApiConfiguration.java
@@ -0,0 +1,24 @@
+package ge.freeuni.informatics.system.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ApiConfiguration {
+
+    @Value("${ge.freeuni.informatics.host}")
+    String host;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins(host);
+            }
+        };
+    }
+}

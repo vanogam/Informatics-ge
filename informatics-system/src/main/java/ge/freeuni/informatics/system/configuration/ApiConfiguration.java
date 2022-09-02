d196346d083cd9b97147a90b48b4a5ee0a7acd7e
Vano Gamezardashvili <vgame18@freeuni.edu.ge>
2022-09-02 23:39:19 +0400

    added cors policy for logout.

--- informatics-system/src/main/java/ge/freeuni/informatics/system/configuration/ApiConfiguration.java
+++ informatics-system/src/main/java/ge/freeuni/informatics/system/configuration/ApiConfiguration.java
@@ -5,13 +5,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ApiConfiguration {
 
-//    @Value("${ge.freeuni.informatics.host}")
-//    String host;
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
     @Bean
     public CorsFilter corsFilter() {
 

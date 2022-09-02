be43ee8b7e35aaa0d41fcd0f67b1446b3b2c3d78
Vano Gamezardashvili <vgame18@freeuni.edu.ge>
2022-09-02 23:28:10 +0400

    changed cors filter.

--- informatics-system/src/main/java/ge/freeuni/informatics/system/configuration/ApiConfiguration.java
+++ informatics-system/src/main/java/ge/freeuni/informatics/system/configuration/ApiConfiguration.java
@@ -1,10 +1,10 @@
 package ge.freeuni.informatics.system.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class ApiConfiguration {
@@ -13,12 +13,19 @@ public class ApiConfiguration {
 //    String host;
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
-            }
-        };
+    public CorsFilter corsFilter() {
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // you USUALLY want this
+        // likely you should limit this to specific origins
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/logout", config);
+        return new CorsFilter(source);
     }
 }

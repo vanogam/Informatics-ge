1fd81360378a27e85bf693c3eb5c65ee3eb0dabe
Anastasia Kvinikadze <akvinikadze+1@impel.io>
2022-09-04 14:42:26 +0400

    style changes

--- informatics-ui/src/Pages/Submissions.jsx
+++ informatics-ui/src/Pages/Submissions.jsx
@@ -14,83 +14,27 @@ import TableBody from "@mui/material/TableBody";
 import Typography from "@mui/material/Typography";
 import TableContainer from "@mui/material/TableContainer";
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
 export default function Submissions() {
   const [submissions, setSubmissions] = useState([]);
   const [selectedSubmission, setSelectedSubmission] = useState({});
   const [popUp, setPopUp] = useState(false);
   const { contest_id } = useParams();
-//   const something = {
-//     message: null,
-//     status: "SUCCESS",
-//     submissions: [
-//       {
-//         id: 466,
-//         cmsId: 12,
-//         userId: 38,
-//         username: "datiko",
-//         text: "   #include <bits/stdc++.h>\n\nusing namespace std;\n\nint n;\nint cnt[200];\nbool blocked[200];\nstring s;\n\nchar get_min(int i){\n    int mx = n+1;\n    char ret;\n    for(char c='a'; c<='z'; c++){\n        if(blocked[c])\n            continue;\n        if(cnt[c] < mx)\n        {\n            mx = cnt[c];\n            ret = c;\n        }\n    }\n    return ret;\n}\n\nvoid repl(int i){\n    for(char c='a'; c<='z'; c++){\n        blocked[c] = false;\n    }\n    if(i-1>=0) blocked[s[i-1]] = true;\n    if(i-2>=0) blocked[s[i-2]] = true;\n    blocked[s[i]] = true;\n    if(i+1<n) blocked[s[i+1]] = true;\n    if(i+2<n) blocked[s[i+2]] = true;\n    cnt[s[i]]--;\n    s[i] = get_min(i);\n    cnt[s[i]]++;\n}\n\nmain(){\n\n    cin>>s;\n    n=s.size();\n\n    for(int i=0;i<n;i++){\n        cnt[s[i]]++;\n    }\n\n    for(int i=2;i<n;i++){\n        if(s[i]==s[i-1] && s[i]==s[i-2])\n        {\n            repl(i);\n        }\n    }\n\n    for(char c='a'; c<='z'; c++){\n        if(cnt[c] > n/2)\n        {\n            for(int i=0;i<n;i++){\n                if(s[i]==c)\n                    repl(i);\n                if(cnt[c] <= n/2)\n                    break;\n            }\n        }\n    }\n\n    cout<<s<<endl;\n\n}",
-//         status: "FINISHED",
-//         currentTest: 26,
-//         score: 100.0,
-//         taskId: 440,
-//         contestId: 439,
-//         language: "CPP",
-//         submissionTime: "2022-09-03T13:52:03.023+00:00",
-//         compilationResult: "ok",
-//         compilationMessage:
-//           "passw.cpp:39:6: warning: ISO C++ forbids declaration of 'main' with no type [-Wreturn-type]\n   39 | main(){\n      |      ^",
-//         results: [
-//           {
-//             idx: "01",
-//             outcome: "Correct",
-//             text: "Output is correct",
-//             time: 0.0,
-//             memory: 200704,
-//           },
-//           {
-//             idx: "02",
-//             outcome: "Correct",
-//             text: "Output is correct",
-//             time: 0.0,
-//             memory: 208896,
-//           },
-//           {
-//             idx: "03",
-//             outcome: "Correct",
-//             text: "Output is correct",
-//             time: 0.0,
-//             memory: 208896,
-//           },
-//           {
-//             idx: "04",
-//             outcome: "Correct",
-//             text: "Output is correct",
-//             time: 0.0,
-//             memory: 200704,
-//           },
-//           {
-//             idx: "05",
-//             outcome: "Correct",
-//             text: "Output is correct",
-//             time: 0.0,
-//             memory: 200704,
-//           },
-//         ],
-//       },
-//     ],
-//   };
+
   const hightlightWithLineNumbers = (input, grammar, language) =>
     highlight(input, grammar, language)
       .split("\n")
       .map((line, i) => `${line}`)
       .join("\n");
-  axios
-    .get(`http://localhost:8080/contest/${contest_id}/submissions`)
-    .then((response) => {
-      if (response.data.status === "SUCCESS")
-        setSubmissions(response.data.submissions);
-      else return <>NO SUBMISSIONS FOUND</>;
-    });
+      useEffect(() => {
+        axios
+          .get(`http://localhost:8080/contest/${contest_id}/submissions`)
+          .then((response) => {
+            if (response.data.status === "SUCCESS")
+              setSubmissions(response.data.submissions);
+            else return <>NO SUBMISSIONS FOUND</>;
+          });
+      }, []);
   return (
     <>
       <Typography 	sx={{ color: '#452c54', fontWeight: 'bold' }} align="center" variant="h6" mb="1rem" mt="1rem">
@@ -103,7 +47,7 @@ export default function Submissions() {
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Contest / Task</TableCell>
+              <TableCell>Contest | Task</TableCell>
               <TableCell align="right">Username</TableCell>
               <TableCell align="right">Submission Time</TableCell>
               <TableCell align="right">Language</TableCell>
@@ -125,12 +69,16 @@ export default function Submissions() {
                 }}
               >
                 <TableCell component="th" scope="row">
-                  {submission.contestId} / {submission.taskId}
+                  {submission.contestId} | {submission.taskId}
                 </TableCell>
                 <TableCell align="right">{submission.username}</TableCell>
                 <TableCell align="right">{submission.submissionTime}</TableCell>
                 <TableCell align="right">{submission.language}</TableCell>
-                <TableCell align="right">{submission.status}</TableCell>
+                <TableCell align="right">
+                  {submission.status === "FINISHED"
+                    ? submission.status +" |Score: " +submission.score
+                    : submission.statuss}
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
@@ -145,7 +93,7 @@ export default function Submissions() {
             transform: "translate(-50%, -50%)",
             maxHeight: "80%",
             overflowY: "auto",
-            width: "400px",
+            width: "600px",
             bgcolor: "white",
             border: `2px solid ;`,
             borderRadius: "0.5rem",
@@ -194,26 +142,31 @@ export default function Submissions() {
                   Test cases
                 </Typography>
                 {selectedSubmission?.results?.map((testCase) => (
-                  <Paper elevation={4} sx={{ padding: "1rem" }}>
+                  <Paper elevation={4} sx={{ padding: "1rem" , marginBottom: "1rem",
+                    backgroundColor:
+                      testCase.outcome === "Correct" ? "#CFE8D3" : "#E8CFD4",
+                  }}>
                     <Typography sx={{fontSize: "15px"}}>#{testCase.idx}</Typography>
-                    <Typography sx={{fontSize: "15px"}} >
-                      Status:{" "}
-                      <span style={{ fontWeight: 700 }}>
-                        {testCase.outcome}
-                      </span>
-                    </Typography>
-                    <Typography sx={{fontSize: "15px"}}>
-                      Text:{" "}
-                      <span style={{ fontWeight: 700 }}>{testCase.text}</span>
-                    </Typography>
-                    <Typography sx={{fontSize: "15px"}}>
-                      Time:{" "}
-                      <span style={{ fontWeight: 700 }}>{testCase.time}</span>
-                    </Typography>
-                    <Typography sx={{fontSize: "15px"}}>
-                      Memory:{" "}
-                      <span style={{ fontWeight: 700 }}>{testCase.memory}</span>
-                    </Typography>
+                    <Box sx={{display: "flex", justifyContent: "space-between"}}>
+                        <Typography sx={{fontSize: "15px"}} >
+                          Status:{" "}
+                          <span style={{ fontWeight: 700 }}>
+                            {testCase.outcome}
+                          </span>
+                        </Typography>
+                        <Typography sx={{fontSize: "15px"}}>
+                          Message:{" "}
+                          <span style={{ fontWeight: 700 }}>{testCase.text}</span>
+                        </Typography>
+                        <Typography sx={{fontSize: "15px"}}>
+                          Time:{" "}
+                          <span style={{ fontWeight: 700 }}>{testCase.time}</span>
+                        </Typography>
+                        <Typography sx={{fontSize: "15px"}}>
+                          Memory:{" "}
+                          <span style={{ fontWeight: 700 }}>{parseInt(testCase.memory/1000) + "KB"}</span>
+                        </Typography>
+                    </Box>
                   </Paper>
                 ))}
               </Paper>
